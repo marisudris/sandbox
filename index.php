ffdbdf64dc88874e6eedf6bed4a640709996c00b
Maris Udris <maris.udris.lv@gmail.com>
2021-02-25 23:12:48 +0200

    Added to the setup in index.php

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 declare(strict_types=1);
 
+use App\Client;
+use App\SourceA;
+use App\SourceB;
+use App\SourceInterface;
+use League\Container\Container;
+
 require __DIR__ . '/vendor/autoload.php';
 
+$container = new Container();
+$container->add(SourceInterface::class, SourceB::class);
+$container->add(Client::class)->addArgument(SourceInterface::class);
 
+$client = $container->get(Client::class);
+echo $client->run();
